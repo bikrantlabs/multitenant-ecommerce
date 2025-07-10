02a7fc15b6c27dfbb86dc78fb1145bbdf8764366
bikrantj <bikrant11bit23@kcc.edu.np>
2025-07-10 22:38:04 +0545

    neo-brutalism

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,5 +40,5 @@ export default antfu(
       "test/padding-around-all": "error", // Add padding in test files
       "test/prefer-lowercase-title": "off", // Allow using uppercase titles in test titles
     },
-  }
+  },
 );
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Button>Click Me</Button>
+      <Input />
+    </div>
+  );
+}
